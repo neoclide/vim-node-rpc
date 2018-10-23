50a64eb9e9b29f176e1373332738dd76b9e45476
chemzqm <chemzqm@gmail.com>
2018-10-24 02:04:07 +0800

    fix notification of nvim_buf_set_var

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,9 @@ server.on('notification', (event, args) => {
   if (!conn.isReady) return
   if (event == 'nvim_call_function') {
     conn.call(true, args[0], args[1])
-  } else if (event == 'nvim_buf_set_var') {
-    conn.commmand(args[0])
   } else if (event == 'nvim_command') {
+    conn.commmand(args[0])
+  } else if (event == 'nvim_buf_set_var') {
     conn.call(true, 'setbufvar', [args[0].id, args[1], args[2]])
   } else {
     logger.error(`Unknown event:`, event, args)
